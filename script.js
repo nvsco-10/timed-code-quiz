aaae86dcb643b1327f0873035579710dcb5b9f99
Neema Velasco <neemavelasco@gmail.com>
2022-03-07 15:12:26 -0600

    added code questions array structure

---
+++ script.js
@@ -0,0 +1,54 @@
+
+// Javascript interview questions source: https://www.guru99.com/javascript-interview-questions-answers.html
+const questions = [
+    {
+        question: "Which of the following is NOT a Javascript data type?",
+        choices: ["String", "NaN", "Boolean", "Undefined"],
+        answer: "NaN"
+    },
+    {
+        question: "This is question 2",
+        choices: ["test", "test2", "test 3", "test 4"],
+        answer: "test 3"
+    },
+    {
+        question: "This is question 3",
+        choices: ["hello", "world", "console", "log"],
+        answer: "world"
+    },
+    {
+        question: "Which of the following is NOT a Javascript data type?",
+        choices: ["String", "NaN", "Boolean", "Undefined"],
+        answer: "Nan"
+    },
+    {
+        question: "Which of the following is NOT a Javascript data type?",
+        choices: ["String", "NaN", "Boolean", "Undefined"],
+        answer: "Nan"
+    },
+    {
+        question: "Which of the following is NOT a Javascript data type?",
+        choices: ["String", "NaN", "Boolean", "Undefined"],
+        answer: "Nan"
+    },
+    {
+        question: "Which of the following is NOT a Javascript data type?",
+        choices: ["String", "NaN", "Boolean", "Undefined"],
+        answer: "Nan"
+    },
+    {
+        question: "Which of the following is NOT a Javascript data type?",
+        choices: ["String", "NaN", "Boolean", "Undefined"],
+        answer: "Nan"
+    },
+    {
+        question: "Which of the following is NOT a Javascript data type?",
+        choices: ["String", "NaN", "Boolean", "Undefined"],
+        answer: "Nan"
+    },
+    {
+        question: "Which of the following is NOT a Javascript data type?",
+        choices: ["String", "NaN", "Boolean", "Undefined"],
+        answer: "Nan"
+    },
+]
